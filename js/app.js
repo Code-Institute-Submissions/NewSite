f4b03b6aa77a55d3144ab00e38dfdbdbd64e50a2
Richard Rhodes <rafrhodes@yahoo.co.uk>
2017-12-08 11:49:16 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,27 +3,27 @@ angular.module('Candyland', ['ngRoute', 'RouteControllers', 'UserService', 'Book
 angular.module('Candyland').config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);  // Enable href routing without hashes
     $routeProvider.when('/NewSite/', {
-        templateUrl: '/templates/home.html',
+        templateUrl: 'templates/home.html',
         controller: 'HomeController'
     })
     .when('/home', {
-        templateUrl: '/templates/home.html',
+        templateUrl: 'templates/home.html',
         controller: 'HomeController'
     })
     .when('/packages', {
-        templateUrl: '/templates/packages.html'
+        templateUrl: 'templates/packages.html'
     })
     .when('/gallery', {
-        templateUrl: '/templates/gallery.html'
+        templateUrl: 'templates/gallery.html'
     })
     .when('/sweetlist', {
-        templateUrl: '/templates/sweetlist.html'
+        templateUrl: 'templates/sweetlist.html'
     })
     .when('/bookus', {
-        templateUrl: '/templates/bookus.html',
+        templateUrl: 'templates/bookus.html',
         controller: 'BookingController'
     }).when('/confirm', {
-        templateUrl: '/templates/bookingsuccess.html',
+        templateUrl: 'templates/bookingsuccess.html',
         controller: 'BookingConfirmationController'
     });
 });
