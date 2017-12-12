8449316420880bf0803cd183b090c63b97a5298d
Richard Rhodes <rafrhodes@yahoo.co.uk>
2017-12-12 12:31:53 +0000

    Save hasbankg mode

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 angular.module('Candyland', ['ngRoute', 'RouteControllers', 'UserService', 'BookingDataApi']);
  
 angular.module('Candyland').config(function($routeProvider, $locationProvider) {
-    //$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
     $routeProvider.when('/', {
         templateUrl: 'templates/home.html',
         controller: 'HomeController'
